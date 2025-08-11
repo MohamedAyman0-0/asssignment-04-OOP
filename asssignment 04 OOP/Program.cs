2f1abbcee9999e838c183b7d703eef37065e2fc8
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-11 22:39:06 +0300

    Q1

--- asssignment 04 OOP/Program.cs
+++ asssignment 04 OOP/Program.cs
@@ -1,2 +1,24 @@
-﻿
+﻿#region part01 
+#region Q1
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
 
+    public int Add(int a, int b, int c) => a + b + c;
+
+    public double Add(double a, double b) => a + b;
+}
+#endregion
+#region
+#endregion
+#region
+#endregion
+#region
+#endregion
+#region
+#endregion
+#region
+#endregion
+#region
+#endregion
+#endregion
