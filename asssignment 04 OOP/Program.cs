5eda6af91f5fb1cb7c8539463c01fd81232461fa
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-11 22:40:07 +0300

    Q2

--- asssignment 04 OOP/Program.cs
+++ asssignment 04 OOP/Program.cs
@@ -1,15 +1,41 @@
 ﻿#region part01 
-#region Q1
-public class Calculator
+//#region Q1
+//public class Calculator
+//{
+//    public int Add(int a, int b) => a + b;
+
+//    public int Add(int a, int b, int c) => a + b + c;
+
+//    public double Add(double a, double b) => a + b;
+//}
+//#endregion
+#region Q2
+public class Rectangle
 {
-    public int Add(int a, int b) => a + b;
+    public int Width { get; }
+    public int Height { get; }
 
-    public int Add(int a, int b, int c) => a + b + c;
+   
+    public Rectangle()
+    {
+        Width = 0;
+        Height = 0;
+    }
 
-    public double Add(double a, double b) => a + b;
+   
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    
+    public Rectangle(int side)
+    {
+        Width = side;
+        Height = side;
+    }
 }
-#endregion
-#region
 #endregion
 #region
 #endregion
